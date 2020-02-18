2998b4c298da6ed7addd6aec1d72266b31a4ffc9
Lindsey C <lindseyacason@gmail.com>
2020-02-18 14:06:42 -0600

    updated graph notes

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,30 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+# def earliest_ancestor(ancestors, starting_node):
+#     #array of ancestors
+#     #starting node
+#     #ancestors_list which will keep track of connections
+
+#     '''
+#     Add starting node to the set.
+#     if any 0 position matches starting node move that pair to the set
+#     retrieve the 1 position and call it current_ancestor 
+#     if the current_ancestor matches any 0 position add that pair to the set
+#     set current_ancestor to the 1 position of the new pair passed in, repeat.
+#     if no match, return last pair item in 1 position
+
+
+#     [] make child array, target 9
+#     [8] loop through and add to parent array
+#     [4,11]  4<11 if no matches, retrn less num
+#     '''
+#     parent=list()
+#     child=list()
+
+#     print("starting node ", starting_node)
+#     print("ancestors passed in ", ancestors)
+
+#     for i in ancestors:
+#         if starting_node == i[0]:
+#             parent.append(i[1])
+
+#     print(parent)
--- projects/ancestor/test_ancestor.py
+++ projects/ancestor/test_ancestor.py
@@ -11,6 +11,17 @@ class Test(unittest.TestCase):
       3   5   8
        \ / \   \
         6   7   9
+
+        [3, 1, 10] expecting
+        [6]
+        [3,5]
+        [1,2,4]
+        [10]
+
+        [] make child array, target 9
+        [8] loop through and add to parent array
+        [4,11]  4<11 if no matches, retrn less num
+        
     '''
     def test_earliest_ancestor(self):
         test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
